c587cc7ffbc22d07e892fcdcaac42a9ce3ece93e
egarsan176 <estefaniagarci@gmail.com>
2021-11-16 10:49:08 +0100

    subida proyecto

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <h1>HOLA MUNDO</h1>
+ <?php 
+//  phpinfo();
+ 	echo "<ul>";
+	for ($i=0; $i<10; $i++){
+		echo "<li>$i</li>";
+	}
+	echo "</ul>";
+ ?>
+ </body>
+</html>
